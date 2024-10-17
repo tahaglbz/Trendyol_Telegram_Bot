1e28f057d44898a48fc74b159660224f397ec0f0
tahaglbz <tahaglbz1@gmail.com>
2024-10-17 21:46:14 +0300

    test

--- stream.py
+++ stream.py
@@ -2,10 +2,8 @@ import requests as rq
 from bs4 import BeautifulSoup
 import streamlit as st
 
-# Streamlit UI
 st.title("Create Telegram Bot For Tracking Trendyol Products")
 
-# Information text in English
 st.markdown("""
 ## How to Create a Telegram Bot
 
@@ -31,12 +29,10 @@ st.markdown("""
 6. **Using the Bot**: Enter the token and chat ID in the fields above, then add the Trendyol product URL. Click the "Check Price and Send Telegram Message" button to send a message to your bot.
 """)
 
-# Input fields for bot token, chat ID, and product URL
 bot_token = st.text_input("Enter your Telegram bot token", type="password")
 chat_id = st.text_input("Enter your Telegram chat ID")
 product_url = st.text_input("Enter the Trendyol product URL")
 
-# Function to scrape the product title and latest price
 def get_product_details(url):
     try:
         page = rq.get(url)
@@ -48,20 +44,16 @@ def get_product_details(url):
         st.error(f"Error fetching product details: {e}")
         return None, None
 
-# Function to send message to Telegram
 def send_telegram_message(token, chat_id, message):
     api = f"https://api.telegram.org/bot{token}/sendMessage"
     response = rq.post(url=api, data={"chat_id": chat_id, "text": message})
     return response.json()
 
-# Button to check the product price and send the message
 if st.button("Check Price and Send Telegram Message"):
     if bot_token and chat_id and product_url:
         title, latest_price = get_product_details(product_url)
         if title and latest_price:
-            # Prepare the message to be sent
             message = f"Product: {title}\nLatest Price: {latest_price}"
-            # Send message to Telegram
             send_telegram_message(bot_token, chat_id, message)
             st.success("Message sent to Telegram successfully!")
         else:
